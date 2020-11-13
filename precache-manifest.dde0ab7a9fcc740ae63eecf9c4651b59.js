087e561f6582e82ea0443ee6d4adff368570d346
doopath <maminpahat@gmail.com>
2020-11-14 02:47:20 +0300

    Updates

---
+++ precache-manifest.dde0ab7a9fcc740ae63eecf9c4651b59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4c4bcdecf5cb34ec6d6",
+    "url": "/dooboox/static/css/main.94af430d.chunk.css"
+  },
+  {
+    "revision": "e4c4bcdecf5cb34ec6d6",
+    "url": "/dooboox/static/js/main.d7333c6e.chunk.js"
+  },
+  {
+    "revision": "373cee5feb71ce465ad8",
+    "url": "/dooboox/static/js/runtime~main.03e52b9b.js"
+  },
+  {
+    "revision": "376dc3db8b545d38ebc1",
+    "url": "/dooboox/static/js/2.4e0c2b59.chunk.js"
+  },
+  {
+    "revision": "1878f922ea514226052a",
+    "url": "/dooboox/static/js/3.b735d133.chunk.js"
+  },
+  {
+    "revision": "53d3651ec4da4ef19fec35692473cee7",
+    "url": "/dooboox/index.html"
+  }
+];
