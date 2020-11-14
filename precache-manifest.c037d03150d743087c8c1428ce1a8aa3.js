6f451bf35440446a0d948df1acd089c1ae3c80cf
doopath <maminpahat@gmail.com>
2020-11-14 21:04:51 +0300

    Updates

--- precache-manifest.c037d03150d743087c8c1428ce1a8aa3.js
+++ precache-manifest.c037d03150d743087c8c1428ce1a8aa3.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "46a4d42914c58b747e77",
+    "revision": "c5c81dff3dbb6b1ed4a1",
     "url": "/dooboox/static/css/main.1a7ad7bf.chunk.css"
   },
   {
-    "revision": "46a4d42914c58b747e77",
-    "url": "/dooboox/static/js/main.dc19cd17.chunk.js"
+    "revision": "c5c81dff3dbb6b1ed4a1",
+    "url": "/dooboox/static/js/main.fd550586.chunk.js"
   },
   {
     "revision": "3d5c66f966217ff5fa40",
     "url": "/dooboox/static/js/runtime~main.c2baf167.js"
   },
   {
-    "revision": "8f64e3b69f29e63559c7",
-    "url": "/dooboox/static/js/2.79ca16ff.chunk.js"
+    "revision": "4cc908dc975a3a9d0697",
+    "url": "/dooboox/static/js/2.534b4d9e.chunk.js"
   },
   {
     "revision": "d12b117b1944f1d1a9a4",
     "url": "/dooboox/static/js/3.0506ff63.chunk.js"
   },
   {
-    "revision": "afe1ce09a4f42ed3ba70209bc6be071f",
+    "revision": "7a4feb0fff4a0e978b0b9d9cd6999011",
     "url": "/dooboox/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dooboox/precache-manifest.f8fe68bc16eb31381e8fcaea07275719.js"
+  "/dooboox/precache-manifest.c037d03150d743087c8c1428ce1a8aa3.js"
 );
 
 workbox.clientsClaim();
