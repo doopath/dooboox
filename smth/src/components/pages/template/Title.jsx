d527b754b6240cfdcc9a6df00e55d0ae1b94b3f7
doopath <maminpahat@gmail.com>
2020-12-12 17:54:22 +0300

    removed smth dir

--- smth/src/components/pages/template/Title.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export class Title extends Component {
-  render() {
-    return <h2 className="tp-title">{this.props.value}</h2>;
-  }
-}
--- smth/src/store/actionCreator.ts
+++
@@ -1,8 +0,0 @@
-import { store } from "./store";
-
-export const actionCreator = (type: string, payload: any): void => {
-  store.dispatch({
-    type,
-    payload,
-  });
-};
