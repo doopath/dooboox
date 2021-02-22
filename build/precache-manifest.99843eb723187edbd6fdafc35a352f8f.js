0e97d9d2aad66ce031aab550de4cedb4342fd7c3
doopath <maminpahat@gmail.com>
2021-02-22 10:04:52 +0300

    a commit before mergin with the develop branch

---
+++ build/precache-manifest.99843eb723187edbd6fdafc35a352f8f.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "a737099a96943e3007be",
+    "url": "/dooboox/static/css/main.50d6a52c.chunk.css"
+  },
+  {
+    "revision": "a737099a96943e3007be",
+    "url": "/dooboox/static/js/main.8cc1e1b8.chunk.js"
+  },
+  {
+    "revision": "fd5a096f7532901350b8",
+    "url": "/dooboox/static/js/runtime~main.10fc229c.js"
+  },
+  {
+    "revision": "2db0edb80dad611e78bb",
+    "url": "/dooboox/static/css/2.20cad54e.chunk.css"
+  },
+  {
+    "revision": "2db0edb80dad611e78bb",
+    "url": "/dooboox/static/js/2.02e758f6.chunk.js"
+  },
+  {
+    "revision": "cff7249d17e9ef188d22",
+    "url": "/dooboox/static/js/3.2709a808.chunk.js"
+  },
+  {
+    "revision": "6ce63a3a3a5573fa922ceb1b3a360d73",
+    "url": "/dooboox/static/media/tux.6ce63a3a.svg"
+  },
+  {
+    "revision": "9ddec8eb6c6f0b24955c9e704a05ec69",
+    "url": "/dooboox/static/media/reactLogo.9ddec8eb.svg"
+  },
+  {
+    "revision": "98206e1ca712845159f663a45de6c15d",
+    "url": "/dooboox/static/media/fish.98206e1c.jpeg"
+  },
+  {
+    "revision": "66bfcb92ab707a603fbda186abd3d5b0",
+    "url": "/dooboox/static/media/nonePage.66bfcb92.png"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/dooboox/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/dooboox/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/dooboox/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/dooboox/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/dooboox/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/dooboox/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/dooboox/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/dooboox/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/dooboox/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/dooboox/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/dooboox/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/dooboox/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/dooboox/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/dooboox/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/dooboox/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/dooboox/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/dooboox/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/dooboox/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/dooboox/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/dooboox/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/dooboox/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/dooboox/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/dooboox/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/dooboox/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "f71395139e629fae6c7df7b0f637d91c",
+    "url": "/dooboox/index.html"
+  }
+];
