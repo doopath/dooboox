bbd1ff0c8cdc1d29554b459908e9fd6d5c78dcea
Michael Nikishov <doopath@gmail.com>
2021-03-29 17:44:56 +0300

    moved to new sdk

---
+++ src/components/pages/Engines/PaginationButtonEngine.tsx
@@ -0,0 +1,52 @@
+import { Engine } from "./Engine"
+import React from "react"
+
+export class PaginationButtonEngine implements Engine {
+  private type: "previous" | "next"
+  private switchingPageId: string
+  private button: JSX.Element
+  private buttonContent: string
+  private onClick: Function | false
+
+  public constructor(
+    type: "previous" | "next",
+    switchingPageId: string,
+    onClick?: Function
+  ) {
+    this.type = type
+    this.button = <div></div>
+    this.onClick = onClick || false
+
+    let prefix = switchingPageId.split("_")[0]
+    let suffix = switchingPageId.split("_")[1]
+
+    if (type === "previous") {
+      this.buttonContent = "Previous"
+      this.switchingPageId = String(Number(prefix) - 1) + "_" + suffix
+    } else {
+      this.buttonContent = "Next"
+      this.switchingPageId = String(Number(prefix) + 1) + "_" + suffix
+    }
+  }
+
+  public create = (): void => {
+    this.button = (
+      <div
+        className={`tp-pagination_button ${this.type}-button`}
+        onClick={(): void => {
+          if (this.onClick) {
+            this.onClick(this.switchingPageId)
+          }
+        }}
+      >
+        <a href="#">
+          <span>{this.buttonContent}</span>
+        </a>
+      </div>
+    )
+  }
+
+  public getElement = (): JSX.Element => {
+    return this.button
+  }
+}
---
+++ src/components/pages/Engines/PaginationEngine.tsx
@@ -0,0 +1,111 @@
+import React from "react"
+import * as axios from "axios"
+
+import { Engine } from "./Engine"
+import { PaginationButtonEngine } from "./PaginationButtonEngine"
+import { cacher } from "../../modules/Cacher"
+import { Notification } from "../template/Notification"
+
+export class PaginationEngine implements Engine {
+  private props: object
+  private setPage: (pageId?: string) => void
+  private setPageSearcherValue: (newValue: number | "") => void
+  private pagination: JSX.Element
+  private type: string
+
+  public constructor(props: object) {
+    this.props = props
+    this.pagination = <></>
+    this.type = props["type"]
+    this.setPage = props["setPage"]
+    this.setPageSearcherValue = props["setPageSearcherValue"]
+  }
+
+  public getElement = (): JSX.Element => {
+    return this.pagination
+  }
+
+  private checkPage = async (modify: Function, pageId?: string): Promise<void> => {
+    await axios["get"](this.props["api"].getPageUrl(this.type, pageId || false))
+      .then((response: object) => {
+        if (response["data"]) {
+          modify(true)
+          cacher.save(response["data"][0]["id"], response["data"][0])
+        }
+      })
+      .catch(() => {})
+  }
+
+  private searchPage = async (targetId: string): Promise<boolean> => {
+    let pageExists: boolean | undefined = undefined
+    await this.checkPage((state: boolean) => (pageExists = state), targetId)
+
+    return pageExists || false
+  }
+
+  private setSearchingPage = async (): Promise<void> => {
+    let target: string = this.props["pageSearcherValue"] + "_" +
+      this.props["currentPageId"].split("_")[1],
+      message: string,
+      result = await this.searchPage(target)
+
+    if (result) {
+      this.setPage(target)
+      message = `Switched to the page with id=${this.props["pageSearcherValue"]}`
+    } else {
+      message = `Page with id=${this.props["pageSearcherValue"]} does not exists!`
+    }
+
+    this.props["actionCreator"]("ADD_NEW_NOTIFICATION", new Notification({message}))
+  }
+
+  public create = (): void => {
+    let pageId: string = this.props["pageId"],
+      nextPage: boolean = this.props["nextPage"],
+      previous, next,
+      numberId: number = Number(pageId.split("_")[0]) // Take '1' from '1_PageName'
+
+    if (typeof numberId === "number") {
+      if (numberId >= 2) {
+        previous = new PaginationButtonEngine("previous", pageId, this.setPage)
+        previous.create()
+        previous = previous.getElement()
+      }
+
+      if (nextPage) {
+        next = new PaginationButtonEngine("next", pageId, this.setPage)
+        next.create()
+        next = next.getElement()
+      }
+    }
+
+    this.pagination = (
+      <div className="tp-pagination_block">
+        {previous}
+        {next}
+        <div className="tp-page-searcher">
+          <input
+            type="text"
+            id="pageSearcher"
+            placeholder="Page number"
+            value={this.props["pageSearcherValue"]}
+            onChange={(e): void =>
+              this.setPageSearcherValue(Number(e.target.value) || "")
+            }
+          />
+          <div
+            key={this.props["getUniqueKey"]()}
+            className="tp-page-searcher_button"
+            onClick={(): void => {
+              this.setSearchingPage()
+            }}
+          >
+            <a href="#" key={this.props["getUniqueKey"]()}>
+              <span>Search</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
